3ba2832a9090a5d35e56ca097fe4f8ed58b86b89
Rahel Joshi <ci0n3@Rahels-MacBook-Air.local>
2022-04-11 18:05:57 -0500

    updated

--- turtlesnake.py
+++ turtlesnake.py
@@ -10,8 +10,12 @@ screen = pygame.display.set_mode((x, y))
 sets up the screen size
 """
 
-turtle = pygame.image.load(os.getcwd() + r"\turtle.png")
-cherry = pygame.image.load(os.getcwd() + "\cherry.png")
+import sys
+print(sys.path)
+print(sys.version)
+
+turtle = pygame.image.load(os.getcwd() + r"/turtle.png")
+cherry = pygame.image.load(os.getcwd() + "/cherry.png")
 """
 imports images
 """
@@ -28,23 +32,28 @@ font = pygame.font.SysFont("Comic Sans MS", 30)
 sets up the score text
 """
 
-pygame.draw.line(screen, (128, 128, 128), (0, 71), (x, 71), 144)
+speed = 5
+"""
+makes faster
+"""
+
+pygame.draw.line(screen, (128, 128, 128), (0, 35), (x, 35), 72)
 pygame.display.flip()
 """
 sets up the border between playable area and scoreboard
 """
 
-pos_fruit = [random.randint(0, x-68), random.randint(72, y-52)]
+pos_fruit = [random.randint(0, x-68), random.randint(75, y-52)]
 """
 places fruit at random spot
 """
 
 while True:
     if abs(pos[0]-pos_fruit[0]) < 35 and abs(pos[1]-pos_fruit[1]) < 35:
-        pygame.draw.line(screen, (128, 128, 128), (0, 71), (x, 71), 144)
+        pygame.draw.line(screen, (128, 128, 128), (0, 35), (x, 35), 72)
         pygame.display.flip()
         score += 1
-        pos_fruit = [random.randint(0, x - 68), random.randint(72, y - 52)]
+        pos_fruit = [random.randint(0, x - 68), random.randint(75, y - 52)]
     """
     if the turtle is next to the fruit, increments the score and places fruit at random spot
     """
@@ -58,14 +67,18 @@ while True:
     """
 
     key = pygame.key.get_pressed()
-    if key[pygame.K_w] and pos[1] > 72:
-        pos[1] -= 1
-    elif key[pygame.K_s] and pos[1] < y-52:
-        pos[1] += 1
-    elif key[pygame.K_a] and pos[0] > 0:
-        pos[0] -= 1
+    if key[pygame.K_w] and pos[1] - speed >= 75:
+        pos[1] -= speed
+    elif key[pygame.K_s] and pos[1] + speed < y-52:
+        pos[1] += speed
+    elif key[pygame.K_a] and pos[0] > 1:
+        pos[0] -= speed
     elif key[pygame.K_d] and pos[0] < x-68:
-        pos[0] += 1
+        pos[0] += speed
+    elif key[pygame.K_q] and speed > 1:
+        speed -= 1
+    elif key[pygame.K_e] and speed < 25:
+        speed += 1
     """
     checks for wasd
     """
